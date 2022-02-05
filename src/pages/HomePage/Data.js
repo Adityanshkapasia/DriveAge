a2676ac52ba48eb227ed71f029e62a21f4ca66ef
Devansh-Goel <devanshgoel2000@gmail.com>
2022-02-04 20:44:27 -0500

    Landing Page

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,54 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'DriveAge',
+  headline: 'One stop destination for all your car needs',
+  description:
+    '',
+  buttonLabel: 'Enquire Now',
+  imgStart: '',
+  img: require('../../images/svg-1.svg'),
+  alt: '',
+  start: ''
+};
+
+export const homeObjTwo = {
+//   primary: true,
+//   lightBg: false,
+//   lightTopLine: true,
+//   lightText: true,
+//   lightTextDesc: true,
+//   topLine: 'Instant Setup',
+//   headline: 'Extremely quick onboarding process',
+//   description:
+//     "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
+//   buttonLabel: 'Learn More',
+//   imgStart: '',
+//   img: require('../../images/svg-2.svg'),
+//   alt: 'Vault',
+//   start: ''
+};
+
+
+
+export const homeObjFour = {
+  // primary: true,
+  // lightBg: false,
+  // lightTopLine: true,
+  // lightText: true,
+  // lightTextDesc: true,
+  // topLine: 'Secure Database',
+  // headline: 'All your data is stored on our secure server',
+  // description:
+  //   'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
+  // buttonLabel: 'Sign Up Now',
+  // imgStart: 'start',
+  // img: require('../../images/svg-3.svg'),
+  // alt: 'Vault',
+  // start: 'true'
+};
+
+
