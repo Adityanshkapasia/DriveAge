63d5e8251da1f5d989a8795ddcae84aa5ce4670d
VivaanGoomer <vivaangoomer@gmail.com>
2022-03-12 16:21:13 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import GlobalStyle from './globalStyles';
 import Home from './pages/HomePage/Home';
 import Services from './pages/Services/Services';
 import Products from './pages/Products/Products';
-import SignUp from './pages/SignUp/SignUp';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import { Navbar, Footer } from './components';
 import'./App.css'
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
         <Route path='/' exact component={Home} />
         <Route path='/services' component={Services} />
         <Route path='/products' component={Products} />
-        <Route path='/sign-up' component={SignUp} />
+        <Route path='/SignUp' component={SignUp} />
       </Switch>
       <Footer />
     </Router>
