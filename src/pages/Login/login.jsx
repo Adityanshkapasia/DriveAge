b44884022d5b254b06dc7769a35c68673f5d7cff
VivaanGoomer <vivaangoomer@gmail.com>
2022-04-12 07:47:21 -0400

    updated login page

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -4,13 +4,26 @@ import { Button, Container } from '@material-ui/core'
 import { useForm } from 'react-hook-form'
 
 export default function LoginPage() {
-  const {register,handleSubmit} = useForm();
-  const onSubmit=(data) => console.log(data)
+  const {register,
+    handleSubmit,
+     formState: {errors}
+    } = useForm();
+  const onSubmit=(data) => console.log(data);
   return (
     <Container maxWidth="sm">
     <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Login Page</h1>
-        <TextField variant='outlined' label="Email" fullWidth autoFocus autoComplete='email'></TextField>
+        <TextField 
+        variant='outlined' label="Email" fullWidth 
+        autoFocus autoComplete='email'
+        {...register("email",{required: "Required field",
+      pattern: { value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+      message: "Invalid email address"}})}
+        error={!!errors?.email}
+        helperText={errors?.email ? errors.email.message : null }
+        >
+
+        </TextField>
     
     <Button variant='contained' color='primary' type='submit'>Login</Button>
     </form>
