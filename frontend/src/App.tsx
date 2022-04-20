18f5bbdcb7653c8465e8fe126c53f45b11c70915
VivaanGoomer <vivaangoomer@gmail.com>
2022-04-20 01:34:42 -0400

    React Routing Fixes #46

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        {/* <Route path="about" element={<About />} /> */}
+      </Routes>
     </div>
+    </div>
+
   );
 }
 
