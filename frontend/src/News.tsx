782ca023291bb9b8f3cfdba92958e5a0b1bf100c
Devansh-Goel <devanshgoel2000@gmail.com>
2022-04-20 14:34:51 -0400

    news error fix

---
+++ frontend/src/News.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+type Allposts= {
+    
+        	id: number,
+               title:   	string,
+               desc: 	string,
+
+               username:  string
+        
+        
+}
+
+export default function News() {
+    const [posts, setposts] = useState<Allposts[]>([])
+    const onLoad=()=>{
+        fetch("http://localhost:8080/news", {
+            credentials : 'include' ,
+        }) .then (res=>{
+            if (res.status=== 200){
+                res.json().then(t=>{
+                    setposts(t)
+                })
+            }
+            else {
+               
+            }
+        })
+    }
+    useEffect(() => {
+      onLoad()
+    
+    }, [])
+    
+    return(
+        <div>
+       {
+           posts.map(post=>
+               (
+             <div key={post.id}>
+                 <Link to={"/ViewPost/"+post.id}>About
+
+                 <h1>
+                     {post.title}
+                 </h1>
+                 author: {post.username}<br></br>
+                 </Link>
+                 -----------------
+             </div>
+             
+               )
+           )
+       }
+        </div>
+    )
+}
