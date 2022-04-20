fb67d4d5ad5c672206af0fe41318ee350eebdccb
ApoorvaPadhi <apoorvapadhi2000@gmail.com>
2022-04-20 15:41:50 -0400

    Update Routes Fixes #100

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Login from './Login';
 import Register from './Register';
 import Viewallblog from './Viewallblog';
 import Createblogpost from './Createblogpost';
 import Viewpost from './Viewpost';
 import Viewallcars from './Viewallcars';
+import Comparecars from './Comparecars';
 import News from './News';
 
 function App() {
-  
+  const navigate=useNavigate()
+  const onLogout=()=>{
+    fetch("http://localhost:8080/logout", {
+        credentials : 'include' ,
+    }) .then (res=>{
+      navigate("/Login")
+        })
+        
+    
+      }
   return (
     <div className="App">
        <div className="App">
@@ -25,8 +35,9 @@ function App() {
         <Link to="/Blog">View All Posts</Link> 
         <Link to="/CreatePost">Create Post</Link>
         <Link to="/cars">View All Cars</Link> 
-        
+        <Link to="/compare">Compare Cars</Link> 
         <Link to="/news">News</Link> 
+        <a onClick={e => onLogout()}>logout</a>
       </nav>
       <Routes>
         <Route path="/Login" element={<Login />} />
@@ -35,7 +46,7 @@ function App() {
         <Route path="/CreatePost" element={<Createblogpost />} />
         <Route path="/ViewPost/:id" element={<Viewpost />} />
         <Route path="/cars" element={<Viewallcars />} />
-     
+        <Route path="/compare" element={<Comparecars />} />
         <Route path="/news" element={<News />} />
       </Routes>
     </div>
